456e19da9018aab2f92e35863d97f0a263b85bac
이선근 <113088135+lee-sungeun@users.noreply.github.com>
2024-09-05 22:07:54 +0900

    Revert "update : 웹서버 연결 데이터 송수신"

--- src/pages/Expert_Insight/components/organisms/OrganismStrategyReportSection.jsx
+++ src/pages/Expert_Insight/components/organisms/OrganismStrategyReportSection.jsx
@@ -11,6 +11,7 @@ import {
   CONVERSATION,
   APPROACH_PATH,
   isLoggedInAtom,
+
 } from "../../../AtomStates";
 import { palette } from "../../../../assets/styles/Palette";
 import images from "../../../../assets/styles/Images";
@@ -70,12 +71,9 @@ const OrganismStrategyReportSection = ({ conversationId, expertIndex }) => {
   ] = useAtom(BUSINESS_INFORMATION_TARGET_CUSTOMER);
   const [buttonState, setButtonState] = useAtom(EXPERT_BUTTON_STATE); // BUTTON_STATE 사용
 
-  const [expert1ReprotData, setExpert1ReportData] =
-    useAtom(EXPERT1_REPORT_DATA);
-  const [expert2ReprotData, setExpert2ReportData] =
-    useAtom(EXPERT2_REPORT_DATA);
-  const [expert3ReprotData, setExpert3ReportData] =
-    useAtom(EXPERT3_REPORT_DATA);
+  const [expert1ReprotData, setExpert1ReportData] = useAtom(EXPERT1_REPORT_DATA);
+  const [expert2ReprotData, setExpert2ReportData] = useAtom(EXPERT2_REPORT_DATA);
+  const [expert3ReprotData, setExpert3ReportData] = useAtom(EXPERT3_REPORT_DATA);
 
   const analysisReportData = {
     title: titleOfBusinessInfo,
@@ -107,20 +105,14 @@ const OrganismStrategyReportSection = ({ conversationId, expertIndex }) => {
     useAtom(strategyReportAtom);
   useEffect(() => {
     const loadData = async () => {
-      console.log("🚀 ~ loadData ~ buttonState:", buttonState);
       if (buttonState === 1) {
-        console.log("🚀 ~ loadData ~ buttonState:", buttonState);
         // BUTTON_STATE가 1일 때만 API 호출
+        setButtonState(0);
         setIsLoading(true);
         setButtonState(0); // BUTTON_STATE를 초기화
-
         try {
-          console.log(
-            "----------------------------------getConversationByIdFromIndexedDB-------------------"
-          );
           const existingConversation = await getConversationByIdFromIndexedDB(
-            conversationId,
-            isLoggedIn
+            conversationId
           );
           const currentReportKey = `strategyReportData_EX${expertIndex}`;
 
@@ -134,7 +126,7 @@ const OrganismStrategyReportSection = ({ conversationId, expertIndex }) => {
             setStrategyReportData(strategyData);
             setTabs(strategyData.tabs);
             setSections(strategyData.tabs[selectedTab].sections);
-          } else if (Object.keys(strategyReportData).length >= 0) {
+          } else if (Object.keys(strategyReportData).length === 0) {
             const data = {
               expert_id: expertIndex,
               business_info: titleOfBusinessInfo, // DB에서 가져온 titleOfBusinessInfo 사용
@@ -189,29 +181,16 @@ const OrganismStrategyReportSection = ({ conversationId, expertIndex }) => {
             setTabs(strategyData.tabs);
             setSections(strategyData.tabs[selectedTab].sections);
 
-            if (strategyData.expert_id === 1)
-              setExpert1ReportData(strategyData);
-            else if (strategyData.expert_id === 2)
-              setExpert2ReportData(strategyData);
-            else if (strategyData.expert_id === 3)
-              setExpert3ReportData(strategyData);
-            console.log(
-              "🚀 ~ loadData ~ existingConversation:",
-              existingConversation
-            );
-
-            existingConversation[currentReportKey] = finalResponse;
+            if(strategyData.expert_id === 1) setExpert1ReportData(strategyData);
+            else if(strategyData.expert_id === 2) setExpert2ReportData(strategyData);
+            else if(strategyData.expert_id === 3) setExpert3ReportData(strategyData);
 
             const updatedConversation = {
               ...existingConversation,
-              // [currentReportKey]: strategyData,
-              // timestamp: Date.now(),
+              [currentReportKey]: strategyData,
+              timestamp: Date.now(),
             };
-            await saveConversationToIndexedDB(
-              updatedConversation,
-              isLoggedIn,
-              conversationId
-            );
+            await saveConversationToIndexedDB(updatedConversation,isLoggedIn,conversationId);
           } else {
             setTabs(strategyReportData.tabs);
             setSections(strategyReportData.tabs[selectedTab].sections);
@@ -221,61 +200,12 @@ const OrganismStrategyReportSection = ({ conversationId, expertIndex }) => {
         }
         setIsLoading(false);
 
-        // const updatedConversation = [...conversation];
-        console.log("🚀 ~ loadData ~ conversation:", conversation);
-
-        // let conversation_list = [...conversation];
-        // conversation_list.push(
-        //   {
-        //     type: "system",
-        //     message:
-        //       "리포트 내용을 보시고 추가로 궁금한 점이 있나요? 아래 키워드 선택 또는 질문해주시면, 더 많은 인사이트를 제공해 드릴게요! 😊",
-        //   },
-        //   { type: `keyword` }
-        // );
-
-        // let updatedConversation = {};
-        // updatedConversation[conversation][updatedConversation] =
-        //   conversation_list;
-
-        // setConversation(updatedConversation);
-        // await saveConversationToIndexedDB(
-        //   updatedConversation,
-        //   isLoggedIn,
-        //   conversationId
-        // );
-        // let conversation_list = [conversation]; // "updatedConversation"를 배열로 감싸기
-
-        const existingConversation_2 = await getConversationByIdFromIndexedDB(
-          conversationId,
-          isLoggedIn
+        const updatedConversation = [...conversation];
+        updatedConversation.push(
+          { type: 'system', message: '리포트 내용을 보시고 추가로 궁금한 점이 있나요? 아래 키워드 선택 또는 질문해주시면, 더 많은 인사이트를 제공해 드릴게요! 😊'},
+          { type: `keyword` },
         );
-
-        let conversationArray = Object.values(conversation);
-
-        conversationArray.push(
-          {
-            type: "system",
-            message:
-              "리포트 내용을 보시고 추가로 궁금한 점이 있나요? 아래 키워드 선택 또는 질문해주시면, 더 많은 인사이트를 제공해 드릴게요! 😊",
-          },
-          { type: `keyword` }
-        );
-        let conversationData = {
-          updatedConversation: conversationArray,
-        };
-        // updatedConversation 객체 생성
-        let updatedConversation = {
-          ...existingConversation_2,
-          conversation: conversationData, // conversation 필드 업데이트
-        };
-
         setConversation(updatedConversation);
-        await saveConversationToIndexedDB(
-          updatedConversation,
-          isLoggedIn,
-          conversationId
-        );
       }
     };
     loadData();
@@ -305,17 +235,17 @@ const OrganismStrategyReportSection = ({ conversationId, expertIndex }) => {
 
         {isLoading ? (
           <>
-            <SkeletonTitle className="title-placeholder" />
-            <SkeletonLine className="content-placeholder" />
-            <SkeletonLine className="content-placeholder" />
-            <Spacing /> {/* 제목과 본문 사이에 간격 추가 */}
-            <SkeletonTitle className="title-placeholder" />
-            <SkeletonLine className="content-placeholder" />
-            <SkeletonLine className="content-placeholder" />
-            <Spacing /> {/* 제목과 본문 사이에 간격 추가 */}
-            <SkeletonTitle className="title-placeholder" />
-            <SkeletonLine className="content-placeholder" />
-            <SkeletonLine className="content-placeholder" />
+          <SkeletonTitle className="title-placeholder" />
+          <SkeletonLine className="content-placeholder" />
+          <SkeletonLine className="content-placeholder" />
+          <Spacing /> {/* 제목과 본문 사이에 간격 추가 */}
+          <SkeletonTitle className="title-placeholder" />
+          <SkeletonLine className="content-placeholder" />
+          <SkeletonLine className="content-placeholder" />
+          <Spacing /> {/* 제목과 본문 사이에 간격 추가 */}
+          <SkeletonTitle className="title-placeholder" />
+          <SkeletonLine className="content-placeholder" />
+          <SkeletonLine className="content-placeholder" />
           </>
         ) : sections.length > 0 ? (
           sections.map((section, index) => (
@@ -345,7 +275,6 @@ const OrganismStrategyReportSection = ({ conversationId, expertIndex }) => {
 };
 
 const Section = ({ title, content }) => {
-  console.log("🚀 ~ Section ~ content:", content);
   // 서브 타이틀이 있는 항목과 없는 항목을 분리
   const subTitleItems = content.filter((item) => item.subTitle);
   const nonSubTitleItems = content.filter((item) => !item.subTitle);
@@ -433,9 +362,9 @@ const AnalysisSection = styled.div`
   }
 
   > p {
-    font-size: 0.875rem;
-    line-height: 1.5;
-    margin-top: 15px;
+    font-size:0.875rem;
+    line-height:1.5;
+    margin-top:15px;
 
     span {
       color: ${palette.red};
@@ -460,9 +389,9 @@ const BoxWrap = styled.div`
   }
 
   p {
-    font-size: 0.875rem;
-    color: ${palette.darkGray};
-    line-height: 1.5;
+    font-size:0.875rem;
+    color:${palette.darkGray};
+    line-height:1.5;
     // margin-bottom:10px;
   }
 `;
@@ -516,7 +445,7 @@ const DynamicGrid = styled.div`
 `;
 
 const SubTitle = styled.strong`
-  font-size: 0.875rem;
+  font-size:0.875rem;
   font-weight: 500;
   color: ${palette.gray};
   text-align: left;
