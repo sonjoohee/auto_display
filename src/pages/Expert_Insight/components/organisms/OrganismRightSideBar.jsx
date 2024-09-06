96ace692a237e7efdef0e46c550c939ee0ef6541
pixelweb <pixelweb@solusbank.com>
2024-09-06 17:25:55 +0900

    우측메뉴(전문가) 이미지 변경, 대화페이지에서 각각 전문가의 이미지가 연동되야함

--- src/pages/Expert_Insight/components/organisms/OrganismRightSideBar.jsx
+++ src/pages/Expert_Insight/components/organisms/OrganismRightSideBar.jsx
@@ -29,7 +29,7 @@ const OrganismRightSideBar = () => {
             <AIProfile>
               <div className="profileInfo">
                 <div className="thumb">
-                  <img src={panelimages.expert_1} alt="" />
+                  <img src={selectedExpert.images} alt="" />
                 </div>
                 <div className="name">
                   <strong>{selectedExpert.title}</strong>
