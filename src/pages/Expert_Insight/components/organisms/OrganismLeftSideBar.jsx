02fc42e4e37bb28190a62808575a072a13a8f4ef
unknown <sjjjang00@gmail.com>
2024-09-26 15:03:36 +0900

    히스토리 대화 구분 문구 수정

--- src/pages/Expert_Insight/components/organisms/OrganismLeftSideBar.jsx
+++ src/pages/Expert_Insight/components/organisms/OrganismLeftSideBar.jsx
@@ -1009,7 +1009,7 @@ useEffect(() => {
                     )}
                     {chatList.some(chat => (Date.now() - chat.timestamp) > 604800000 && (Date.now() - chat.timestamp <= 2592000000)) && (
                       <>            
-                      <strong>지난 7일</strong>
+                      <strong>지난 7일 대화</strong>
                         <ul>
                           {chatList
                             .filter(chat => Date.now() - chat.timestamp > 604800000 && Date.now() - chat.timestamp <= 2592000000)
@@ -1100,7 +1100,7 @@ useEffect(() => {
                     )}
                     {chatList.some(chat => Date.now() - chat.timestamp > 2592000000) && (
                       <>            
-                      <strong>지난 30일</strong>
+                      <strong>지난 30일 대화</strong>
                         <ul>
                           {chatList
                             .filter(chat => Date.now() - chat.timestamp > 2592000000)
