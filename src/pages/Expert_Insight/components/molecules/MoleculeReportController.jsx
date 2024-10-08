17826935f60ea61c32d01ed3f3d48115bd8392cb
unknown <sjjjang00@gmail.com>
2024-10-08 12:04:38 +0900

    복사 줄바꿈

--- src/pages/Expert_Insight/components/molecules/MoleculeReportController.jsx
+++ src/pages/Expert_Insight/components/molecules/MoleculeReportController.jsx
@@ -534,12 +534,12 @@ ${mainCharacteristicOfBusinessInformation
         contentToCopy = extractTextContent(customerAdditionalReportData[additionalReportCount]);
       } else if (reportIndex === 4) {
         if (recommendedTargetData && recommendedTargetData.poc_persona) {
-          contentToCopy = "PoC 목적별 추천 타겟 및 예상 인사이트\n";
+          contentToCopy = "PoC 목적별 추천 타겟 및 예상 인사이트\n\n";
           Object.entries(recommendedTargetData.poc_persona).forEach(([key, value], index) => {
             const goalActionText = findGoalActionText(index);
             contentToCopy += `${index + 1}. ${goalActionText}\n`;
             contentToCopy += `1. 추천 가상 페르소나 : ${value[0]["추천 가상 페르소나"]}\n`;
-            contentToCopy += `2. 이유 및 예상 인사이트 : ${value[1]["이유 및 예상 인사이트"]}\n`;
+            contentToCopy += `2. 이유 및 예상 인사이트 : ${value[1]["이유 및 예상 인사이트"]}\n\n`;
           });
         }
       }
