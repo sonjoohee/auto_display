64da53b9cade9ddd0f7a7c9ae9e67cc8020e815c
pixelweb <pixelweb@solusbank.com>
2025-01-24 18:49:29 +0900

    1:1의 경우에만 페르소나 비활성화

--- src/pages/Persona/components/molecules/MoleculePersonaSelectCard.jsx
+++ src/pages/Persona/components/molecules/MoleculePersonaSelectCard.jsx
@@ -109,6 +109,7 @@ const MoleculePersonaSelectCard = ({
           key={persona.persona_id}
           selected={true}
           anySelected={true}
+          interviewType={interviewType}
         >
           {/* 카드 내용 렌더링 */}
           <Persona color="Linen" size="Large" Round>
@@ -155,6 +156,7 @@ const MoleculePersonaSelectCard = ({
           key={persona.persona_id}
           selected={false}
           anySelected={personaListState.selected.length > 0}
+          interviewType={interviewType}
         >
           {/* 카드 내용 렌더링 */}
           <Persona color="Linen" size="Large" Round>
@@ -214,13 +216,14 @@ const ListBoxItem = styled.div`
   padding: 16px;
   border-radius: 10px;
   background: ${(props) => props.theme.white};
-  // border: 1px solid
-  /* ${(props) =>
-    props.selected ? palette.outlineGray : palette.outlineGray}; */
-  opacity: ${(props) => (props.anySelected && !props.selected ? 0.5 : 1)};
+  opacity: ${(props) =>
+    props.interviewType === "single" && props.anySelected && !props.selected
+      ? 0.5
+      : 1};
 
   + div {
     border-top: 1px solid ${palette.outlineGray};
+    border-radius: 0;
   }
 `;
 
