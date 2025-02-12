4ad54d3b5185d3cc5b1ae243e9957a6bea07eb1d
lee-sungeun <sungeun_lee@userconnect.kr>
2025-02-12 15:30:40 +0900

    fix : 날짜 템플릿 수정

--- src/pages/Persona/components/pages/PagePersona4Single.jsx
+++ src/pages/Persona/components/pages/PagePersona4Single.jsx
@@ -325,6 +325,12 @@ const PagePersona4 = () => {
                     createTimestamp: new Date().getTime(),
                     createDate: new Date().toLocaleString("ko-KR", {
                       timeZone: "Asia/Seoul",
+                      year: "numeric",
+                      month: "2-digit",
+                      day: "2-digit",
+                      hour: "2-digit",
+                      minute: "2-digit",
+                      second: "2-digit",
                     }),
                   },
                 ],
