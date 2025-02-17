d8112ce44d36266c063c92690b0efaea10367f22
pixelweb0 <pixelweb@naver.com>
2025-02-17 13:44:31 +0900

    아이디어 마인드맵 안쪽 텍스트 수평

--- src/components/Charts/ZoomableSunburst.jsx
+++ src/components/Charts/ZoomableSunburst.jsx
@@ -176,6 +176,11 @@ const ZoomableSunburst = ({ data, width, height, colors }) => {
     function labelTransform(d) {
       const x = (d.x0 + d.x1) / 2 * 180 / Math.PI;
       const y = (d.y0 + d.y1) / 2 * radius;
+      // y0가 2보다 작은 경우(안쪽 원)는 수평 유지
+      if (d.y0 < 2) {
+        return `rotate(${x - 90}) translate(${y},0) rotate(${90 - x})`;
+      }
+      // 바깥쪽 원은 기존 로직 유지
       return `rotate(${x - 90}) translate(${y},0) rotate(${x < 180 ? 0 : 180})`;
     }
 
